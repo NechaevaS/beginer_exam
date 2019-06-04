01f557b43e338d539713d8cc1317251c13d66441
Svetlana Nechaeva <snechaev@student.42.us.org>
2019-06-04 13:40:50 -0700

    fixed

--- ft_atoi/ft_atoi.c
+++ ft_atoi/ft_atoi.c
@@ -6,9 +6,24 @@
 /*   By: snechaev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/26 13:51:28 by snechaev          #+#    #+#             */
-/*   Updated: 2019/03/26 15:17:07 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/06/04 13:39:21 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+int ft_strncmp(const char *s1, const char *s2, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (s1[i] != s2[i])
+			return(0);
+		else
+			i++;
+	}
+	return (1);
+}
 
 int	ft_atoi(const char *str)
 {
@@ -19,20 +34,19 @@ int	ft_atoi(const char *str)
 	i = 0;
 	num = 0;
 	neg = 1;
-	while (str[i] != '\0')
+	if (ft_strncmp(str,"-2147483648", 11))
+		return (-2147483648);
+	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
-			i++;
-		if (str[i] == '-' || str[i] == '+')
-		{
-			if(str[i] == '-')
-				neg = -1;
-			i++;
-		}
-		if (str[i] >= '0' && str[i] <= '9')
-		{
-			num = num * 10 + (str[i] - '0');
-		}
+		if(str[i] == '-')
+			neg = -1;
+		i++;
+	}
+	while (str[i] != '\0' && (str[i] >= '0' && str[i] <= '9'))
+	{
+		num = num * 10 + (str[i] - '0');
 		i++;
 	}
 	return (num * neg);
--- ft_atoi/main.c
+++ ft_atoi/main.c
@@ -6,16 +6,35 @@
 /*   By: snechaev <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/26 14:26:19 by snechaev          #+#    #+#             */
-/*   Updated: 2019/03/26 15:21:05 by snechaev         ###   ########.fr       */
+/*   Updated: 2019/06/04 13:39:02 by snechaev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int ft_atoi(const char *str);
+int	ft_atoi(const char *str);
 
-int main()
+int	main(void)
 {
-	printf("%d", ft_atoi("  -3hb45"));
+	printf ("%s\n", "test atoi");
+	printf ("expect %d\n", atoi("-23"));
+	printf ("result %d\n", ft_atoi("-23"));
+	printf ("%s\n", "");
+	printf ("expect %d\n", atoi("23abc"));
+	printf ("result %d\n", ft_atoi("23abc"));
+	printf ("%s\n", "");
+	printf ("expect %d\n", atoi("0"));
+	printf ("result %d\n", ft_atoi("0"));
+	printf ("%s\n", "");
+	printf ("expect %d\n", atoi("2345678"));
+	printf ("result %d\n", ft_atoi("2345678"));
+	printf ("%s\n", "");
+	printf ("expect %d\n", atoi("+2345678"));
+	printf ("result %d\n", ft_atoi("+2345678"));
+	printf ("%s\n", "");
+	printf ("expect %d\n", atoi("-2147483648"));
+	printf ("result %d\n", ft_atoi("-2147483648"));
+	printf ("%s\n", "");
 	return (0);
 }
